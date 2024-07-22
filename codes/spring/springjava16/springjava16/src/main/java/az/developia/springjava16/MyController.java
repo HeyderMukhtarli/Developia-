a5886d5603893cd0bbb0a006f87da1b9fb7c8dfc
Yusif Alizada <37800222+yalizada@users.noreply.github.com>
2024-07-22 20:44:16 +0400

    25 spring praktika

--- codes/spring/springjava16/springjava16/src/main/java/az/developia/springjava16/MyController.java
+++ codes/spring/springjava16/springjava16/src/main/java/az/developia/springjava16/MyController.java
@@ -1,18 +1,41 @@
 package az.developia.springjava16;
 
+import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/my-controller")
 public class MyController {
 
     @GetMapping("/home")
-    public Book sfkldjkjsdl(@RequestParam(name="sorgu",required = false,defaultValue = "Hasan") String sorgu){
+    public Book sfkldjkjsdl(@RequestParam(name="sorgu",required = false,defaultValue = "Hasan")
+
+                                String sorgu){
 
         return new Book(10,sorgu);
+    }
+
+
+    @GetMapping( value = "/search")
+    public List<Student> search(@RequestParam(name="sorgu",required = false,defaultValue = "Hasan")
+
+                            String sorgu){
+List<Student> students=List.of(
+        new Student("Ad1","Soyad1"),
+        new Student("Ad2","Soyad2"),
+        new Student("Ad3","Soyad3")
+);
+// jackson
+        List<Student> studentsFiltered= students.stream().filter(s->s.getName().contains(sorgu))
+                .collect(Collectors.toList());
+
+        return studentsFiltered;
     }
 
 }
---
+++ codes/spring/springjava16/springjava16/src/main/java/az/developia/springjava16/Student.java
@@ -0,0 +1,27 @@
+package az.developia.springjava16;
+
+public class Student {
+    private String name;
+    private String surname;
+
+    public String getName() {
+        return name;
+    }
+
+    public Student(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+}
