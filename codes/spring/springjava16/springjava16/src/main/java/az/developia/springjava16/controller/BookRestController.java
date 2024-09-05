b41b218f04d2878f1a99299c3ad68dbebfc68cce
Yusif Alizada <37800222+yalizada@users.noreply.github.com>
2024-09-05 21:03:42 +0400

    35 seecurity huquqlar davami

--- codes/spring/springjava16/springjava16/src/main/java/az/developia/springjava16/controller/BookRestController.java
+++ codes/spring/springjava16/springjava16/src/main/java/az/developia/springjava16/controller/BookRestController.java
@@ -29,7 +29,7 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/books")
 @RequiredArgsConstructor
 public class BookRestController {
-
+// field setter const
 	private final BookService service;
 	private final MessageSender sender;
 
---
+++ codes/spring/springjava16/springjava16/src/main/java/az/developia/springjava16/entity/UserEntity.java
@@ -0,0 +1,26 @@
+package az.developia.springjava16.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserEntity {
+
+	@Id
+	private String username;
+
+	private String password;
+
+	private Integer enabled;
+
+}
