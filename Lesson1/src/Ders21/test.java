6855c3e18a1efe57ef64750b8685cf867e25a2ed
heyder04 <haydarmuxtar26@gmail.com>
2024-11-03 03:44:02 +0400

    test

---
+++ Lesson1/src/Ders21/test.java
@@ -0,0 +1,7 @@
+package Ders21;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println(Math.pow(2557,2753)%3233);
+    }
+}
--- library/springjava16/src/main/java/az/developia/springjava16/config/MinioConfig.java
+++ library/springjava16/src/main/java/az/developia/springjava16/config/MinioConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MinioConfig {
-
+ 
     @Value("${minio.url}")
     private String url;
 
